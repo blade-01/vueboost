6dc44f1364b506002db013394b047ab4993a96cc
blade-01 <animashauntaofiq@gmail.com>
2023-01-06 17:57:35 +0100

    Update: cleaned up homepage

--- src/components/home/HomeCTA.vue
+++ src/components/home/HomeCTA.vue
@@ -1,52 +1,13 @@
 <template>
   <div class="flex routes py-8 lg:p-0 h-[calc(100%-78px)] md:h-[calc(100vh-78px)]">
     <div class="container">
-      <div class="flex justify-between items-center flex-col-reverse md:flex-row">
-        <div>
-          <h1
-            class="my-6 md:m-0 font-bold text-[54px] lg:text-[63px] 2xl:text-7xl leading-[65px] sm:max-w-2xl xl:w-[35rem]"
-            data-aos="fade-up"
-            data-aos-anchor-placement="top-bottom"
-          >
-            Welcome to
-            <a href="https://github.com/blade-01" class="underline">Blade's</a>
-            Boilerplate
-          </h1>
-          <div class="flex gap-3">
-            <Icon icon="mdi:youtube" :inline="true" width="96" horizontalFlip="true" />
-            <span class="mdi mdi-youtube"></span>
-            <YouTubeIcon color="green" />
-          </div>
-          <p
-            class="max-w-md leading-[25px] priText text-base mt-2"
-            data-aos="fade"
-            data-aos-delay="500"
-          >
-            Vue 3 + TS + Pinia + TailwindCSS + MDI Fonts + Vue Select
-          </p>
-        </div>
 
-        <div
-          data-aos="fade-up"
-          data-aos-duration="900"
-          data-aos-anchor-placement="top-center"
-        >
-          <figure>
-            <img
-              src="https://avatars.githubusercontent.com/u/47092407?v=4"
-              alt="illustration_01"
-              class="w-full h-full"
-            />
-          </figure>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
-import YouTubeIcon from '~icons/mdi/youtube'
 </script>
 
 <style scoped></style>
--- src/components/navbar/TheHeader.vue
+++ src/components/navbar/TheHeader.vue
@@ -1,72 +1,13 @@
 <template>
   <header
-    class="flex items-center main fixed w-full h-[78px] top-0 left-0 z-50 shadow-sm bg-white"
+    class="bg-white"
   >
     <div class="container">
-      <div class="flex justify-between items-center w-full">
-        <router-link to="/">
-          <img
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-300.png"
-            alt="logo"
-            class="w-[60px] object-contain"
-          />
-        </router-link>
-        <nav
-          class="fixed h-full w-full top-0 -left-full mt-[76px] bg-white py-8 lg:relative lg:w-auto lg:left-0 lg:m-0 lg:p-0 lg:bg-inherit transition-all duration-500 lg:transition-none"
-          :class="{ 'open-navbar': show }"
-        >
-          <ul class="block text-center lg:flex lg:space-x-12 lg:m-0 lg:items-center">
-            <li class="mb-8 lg:m-0 mt-10" v-for="route in routes" :key="route.name">
-              <router-link
-                :to="route.routePath"
-                class="font-medium capitalize"
-                active-class="header-link"
-                >{{ route.name }}</router-link
-              >
-            </li>
-          </ul>
-        </nav>
-        <div
-          class="toggle lg:hidden transition-all duration-500"
-          @click="toggleNav"
-          :class="{ change: show }"
-        >
-          <div class="toggler"></div>
-          <div class="toggler"></div>
-          <div class="toggler"></div>
-        </div>
-      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-const routes: {
-  name: string
-  routePath: string
-}[] = [
-  {
-    name: 'Home',
-    routePath: '/'
-  },
-  {
-    name: 'About',
-    routePath: '/about'
-  }
-]
-const show = ref<boolean>(false)
-
-const toggleNav = (): void => {
-  show.value = !show.value
-}
-
-const router = useRouter()
-
-onMounted(() => {
-  router.beforeEach(() => {
-    show.value = false
-  })
-})
 </script>
 
 <style lang="scss" scoped></style>

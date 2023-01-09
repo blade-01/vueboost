5354291acc4b76426808952593a6fefe2a0ba6f3
blade-01 <animashauntaofiq@gmail.com>
2023-01-09 19:58:18 +0100

    Update: updated about content

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,13 @@ import routes from '~pages';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior() {
+    return {
+      top: 0,
+      behavior: 'smooth'
+    };
+  }
 });
 
 router.beforeEach((to) => {

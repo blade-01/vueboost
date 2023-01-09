3162795ee88b433dd98cdb17ab274a77589098ee
blade-01 <animashauntaofiq@gmail.com>
2023-01-09 23:57:37 +0100

    Docs: Updated README file

--- vite.config.ts
+++ vite.config.ts
@@ -17,44 +17,50 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      strategies: 'injectManifest',
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
-      },
-      injectRegister: 'auto',
-      srcDir: 'src',
-      filename: 'sw.ts',
-      workbox: {
-        sourcemap: true,
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Vue Boost',
-        short_name: 'VueBoost',
-        description: 'Vue Boost description',
-        theme_color: '#1e1e2e',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
       }
     }),
+    // VitePWA({
+    //   strategies: 'injectManifest',
+    //   registerType: 'autoUpdate',
+    //   devOptions: {
+    //     enabled: true
+    //   },
+    //   injectRegister: 'auto',
+    //   srcDir: 'src',
+    //   filename: 'sw.ts',
+    //   workbox: {
+    //     sourcemap: true,
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    //   },
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    //   manifest: {
+    //     name: 'Vue Boost',
+    //     short_name: 'VueBoost',
+    //     description: 'Vue Boost description',
+    //     theme_color: '#1e1e2e',
+    //     icons: [
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable'
+    //       }
+    //     ]
+    //   }
+    // }),
     // https://github.com/webfansplz/vite-plugin-vue-inspector
     Inspector({
       toggleButtonVisibility: 'never'

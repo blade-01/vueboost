e55ec707b6896edce6abd97895def939e32dbd04
blade-01 <animashauntaofiq@gmail.com>
2023-01-10 22:21:51 +0100

    Update: configuring PWA

--- vite.config.ts
+++ vite.config.ts
@@ -41,17 +41,17 @@ export default defineConfig({
         theme_color: '#1e1e2e',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'

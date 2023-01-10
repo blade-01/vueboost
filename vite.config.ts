28700c58e0104e8e53b22379a0b3e190e578db16
blade-01 <animashauntaofiq@gmail.com>
2023-01-10 22:12:07 +0100

    Update: configuring PWA

--- vite.config.ts
+++ vite.config.ts
@@ -16,51 +16,49 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    VitePWA({ injectRegister: 'auto' }),
-    // VitePWA({
-    //   // registerType: 'autoUpdate',
-    //   devOptions: {
-    //     enabled: true,
-    //     type: 'module'
-    //     /* other options */
-    //   },
-    //   injectRegister: 'auto',
-    //   workbox: {
-    //     sourcemap: true,
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    //   },
-    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-    //   manifest: {
-    //     name: 'Vue Boost',
-    //     short_name: 'VueBoost',
-    //     description: 'Vue Boost description',
-    //     theme_color: '#1e1e2e',
-    //     icons: [
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable'
-    //       }
-    //     ]
-    //   },
-    //   strategies: 'injectManifest',
-    //   injectManifest: {
-    //     rollupFormat: 'iife'
-    //   },
-    //   srcDir: 'src',
-    //   filename: 'sw.ts'
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      // workbox: {
+      //   sourcemap: true,
+      //   globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      // },
+      // strategies: 'injectManifest',
+      // injectManifest: {
+      //   rollupFormat: 'iife'
+      // },
+      // srcDir: 'src',
+      // filename: 'sw.ts',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Vue Boost',
+        short_name: 'VueBoost',
+        description:
+          'Boost your productivity with Vue Boost - work smarter, not harder.',
+        theme_color: '#1e1e2e',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
     // https://github.com/webfansplz/vite-plugin-vue-inspector
     Inspector({
       toggleButtonVisibility: 'never'

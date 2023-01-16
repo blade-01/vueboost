9f0d2abcfc50a07b8b8604049f847549a3b2d37d
blade-01 <animashauntaofiq@gmail.com>
2023-01-16 23:51:20 +0100

    Update: changed about icon

--- src/components/navbar/TheFooter.vue
+++ src/components/navbar/TheFooter.vue
@@ -1,26 +1,27 @@
 <template>
-  <footer class="flex gap-3.5 items-center">
-    <RouterLink to="/"><Icon icon="mdi:home" width="23" class="link" /></RouterLink>
-    <div @click="toggleDark()" class="transition-all ease-linear duration-300">
-      <Icon icon="mdi:weather-night" width="23" v-if="isDark" class="link" />
-      <Icon icon="mdi:weather-sunny" width="23" v-else class="link" />
+  <footer>
+    <div class="flex gap-3.5 justify-center items-center">
+      <RouterLink to="/"><Icon icon="mdi:home" width="23" class="link" /></RouterLink>
+      <div @click="toggleDark()" class="transition-all ease-linear duration-300">
+        <Icon icon="mdi:weather-night" width="23" v-if="isDark" class="link" />
+        <Icon icon="mdi:weather-sunny" width="23" v-else class="link" />
+      </div>
+      <Icon icon="mdi:translate" width="23" class="link" @click="toggleLocales" />
+      <RouterLink to="/about"
+        ><Icon icon="fluent:tab-desktop-multiple-20-regular" width="23" class="link"
+      /></RouterLink>
+      <a href="https://github.com/blade-01/vueboost" target="_blank"
+        ><Icon icon="mdi:github" width="23" class="link"
+      /></a>
     </div>
-    <Icon icon="mdi:translate" width="23" class="link" @click="toggleLocales" />
-    <RouterLink to="/about"
-      ><Icon icon="mdi:information-outline" width="23" class="link"
-    /></RouterLink>
-    <a href="https://github.com/blade-01/vueboost" target="_blank"
-      ><Icon icon="mdi:github" width="23" class="link"
-    /></a>
-
-    <div class="absolute bottom-2 left-1/2 -translate-x-1/2">
+    <aside class="text-center mt-5">
       <p class="font-light text-sm">
         Inspired by
-        <a href="https://vitesse.netlify.app" target="_blank" class="underline"
-          >Vitesse</a
+        <a href="https://vueboost.netlify.app" target="_blank" class="underline"
+          >Vue Boost</a
         >
       </p>
-    </div>
+    </aside>
   </footer>
 </template>
 
